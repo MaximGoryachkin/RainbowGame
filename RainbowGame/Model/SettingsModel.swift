6855432d48855a72390f7139e8041c9b70ef9af6
Daniil Kulikovskiy <danilkulikovskiy@gmail.com>
2023-11-17 02:13:27 +0300

    add settings Model

--- RainbowGame/Model/SettingsModel.swift
+++ RainbowGame/Model/SettingsModel.swift
@@ -5,4 +5,27 @@
 //  Created by Daniil Kulikovskiy on 16.11.2023.
 //
 
-import Foundation
+import SwiftUI
+
+final class SettingsModel: ObservableObject {
+    @AppStorage("speedSwitch") var speedSwitch: Double = 2
+    @AppStorage("timeGame") var timeGame: Double = 11
+    @AppStorage("checkedTask") var isCheckedTask: Bool = false
+    @AppStorage("sizeSymbol") var sizeSymbolRow: Double = 12
+    @AppStorage("backgroundSymbol") var backgroundSymbol: Bool = false
+    
+    var backgroundСolor: [String: String] = ["Белый": ".white", "Черный": ".black", "Серый": ".gray",]
+    var defaultBackgroundColor = ".gray"
+    var wordArrangement = ["Случайное", "По центру"]
+    var defaultWordArrangement = "Случайное"
+    
+    var sizeSymbol: CGFloat {
+        get {
+            return CGFloat(sizeSymbolRow)
+        }
+        set {
+            sizeSymbolRow = Double(newValue)
+        }
+    }
+    
+}
